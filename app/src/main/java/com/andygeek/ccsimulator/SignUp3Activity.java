8119d2cb0788e6418626f5c8b17c4d7d895db7e7
AndyGeek <andy.eulogio@gmail.com>
2019-06-16 21:16:06 -0500

    Terminando el diseño

---
+++ app/src/main/java/com/andygeek/ccsimulator/SignUp3Activity.java
@@ -0,0 +1,13 @@
+package com.andygeek.ccsimulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up3);
+    }
+}
