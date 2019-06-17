390385b8299a08dd4ca226c7fea405ec54d98b48
AndyGeek <andy.eulogio@gmail.com>
2019-06-17 03:43:51 -0500

    Upload terminando login

--- app/src/main/java/com/andygeek/ccsimulator/LoginActivity.java
+++ app/src/main/java/com/andygeek/ccsimulator/LoginActivity.java
@@ -1,14 +1,33 @@
 package com.andygeek.ccsimulator;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btn_CrearCuenta;
+    private Button btn_Ingresar;
+    private EditText et_CorreoLogin;
+    private EditText et_PasswordLogin;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn_CrearCuenta = findViewById(R.id.btn_CrearCuenta);
+        btn_Ingresar = findViewById(R.id.btn_Ingresar);
+        et_CorreoLogin = findViewById(R.id.et_CorreoLogin);
+        et_PasswordLogin = findViewById(R.id.et_PasswordLogin);
+        progressDialog = new ProgressDialog(this);
+
+        Initialize();
+
+        btn_Ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignIn(et_CorreoLogin.getText().toString(), et_PasswordLogin.getText().toString());
+                progressDialog.setMessage("Iniciando sesion.....");
+                progressDialog.show();
+            }
+        });
 
         btn_CrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +60,60 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+
+    public void Initialize(){
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Obtengo los datos del usuario que inicio sesión.
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if(firebaseUser != null){
+                    //Alguien ya inicio sesión.
+                }
+                else{
+                    //No hay ningun inicio de sesión
+                }
+            }
+        };
+    }
+
+
+    private void SignIn(String email, String password){
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                    et_CorreoLogin.setText("");
+                    et_PasswordLogin.setText("");
+                    progressDialog.dismiss();
+                    startActivity(intent);
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "No se pudo ingresar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
 }
--- app/src/main/java/com/andygeek/ccsimulator/ProfileActivity.java
+++ app/src/main/java/com/andygeek/ccsimulator/ProfileActivity.java
@@ -1,13 +1,81 @@
 package com.andygeek.ccsimulator;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser firebaseUser;
+
+    private TextView tv_EmailProfile;
+    private LinearLayout ll_CerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tv_EmailProfile = findViewById(R.id.tv_EmailProfile);
+        ll_CerrarSesion = findViewById(R.id.ll_CerrarSesion);
+
+        initialize();
+
+        ll_CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                startActivity(new Intent(getBaseContext(), LoginActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
+            }
+        });
+
+
     }
+
+    public void initialize(){
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Creo un objeto User para mostrar sus datos.
+                firebaseUser = firebaseAuth.getCurrentUser();
+
+                if(firebaseUser != null){
+                    tv_EmailProfile.setText(firebaseUser.getEmail().toString());
+                }
+                else{
+
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    private void signOut(){
+        firebaseAuth.signOut();
+    }
+
 }
