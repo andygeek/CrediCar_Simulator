97e4b3b60a7b8552c72c6027c7959c0db33bb759
AndyGeek <andy.eulogio@gmail.com>
2019-06-17 02:38:46 -0500

    Upload major design

--- app/src/main/java/com/andygeek/ccsimulator/LoginActivity.java
+++ app/src/main/java/com/andygeek/ccsimulator/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.andygeek.ccsimulator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_CrearCuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_CrearCuenta = findViewById(R.id.btn_CrearCuenta);
+
+        btn_CrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/andygeek/ccsimulator/SignUp2Activity.java
+++ app/src/main/java/com/andygeek/ccsimulator/SignUp2Activity.java
@@ -1,13 +1,54 @@
 package com.andygeek.ccsimulator;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SignUp2Activity extends AppCompatActivity {
+import com.andygeek.ccsimulator.model.Usuario;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SignUp2Activity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private Usuario usuario;
+    private EditText et_NombreSignUp;
+    private EditText et_ApellidoSignUp;
+    private EditText et_DniSignUp;
+    private TextView tv_FechaNacSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up2);
+
+        tv_FechaNacSignUp = findViewById(R.id.tv_FechaNacSignUp);
+
+        usuario = new Usuario();
+
+        tv_FechaNacSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePicker_Fragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(c.getTime());
+
+        tv_FechaNacSignUp.setText(currentDateString);
     }
 }
